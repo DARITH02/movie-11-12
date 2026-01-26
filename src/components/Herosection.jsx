5aa169190965d0ce0a01da692a0916092de28232
DARTITH002 <chandarith002@gmail.com>
2026-01-26 12:15:10 +0700

    update hero section

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,20 +1,33 @@
 import React from "react";
-import Bg from "../assets/hero-bg.png"
+import Bg from "../assets/hero-bg.png";
+import Logo from "../assets/logo.png";
+import Hero from "../assets/hero.png";
 
 const Herosection = () => {
   return (
     <>
       <div
-        className="h-screen "
+        className="h-screen flex flex-col items-center"
         style={{
-          background:
-            `url(${Bg})`,
+          background: `url(${Bg})`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "100% 100%",
-          backgroundPosition:"center"
+          backgroundPosition: "center",
         }}
       >
-        
+        <div className="mt-20">
+          <img src={Logo} alt="" />
+        </div>
+
+        <div className="w-full flex justify-center mt-24">
+          <img className="w-2/6" src={Hero} alt="" />
+        </div>
+
+        <div className="mt-9 w-2/5">
+          <span className="text-gray-50 text-5xl font-bold block text-center">
+            Find Movies You’ll Love Without the Hassle
+          </span>
+        </div>
       </div>
     </>
   );
