5e615a8ce2537cfc931246fa1994de08633bd59c
DARTITH002 <chandarith002@gmail.com>
2026-01-27 12:07:06 +0700

    update home

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -19,12 +19,16 @@ const Herosection = () => {
           <img src={Logo} alt="" />
         </div>
 
-        <div className="w-full flex justify-center mt-24">
-          <img className="w-2/6" src={Hero} alt="" />
+        <div className="w-full flex justify-center mt-16 lg:mt-24">
+          <img
+            className="w-4/5 sm:w-4/5 md:w-2/3 lg:w-2/4 xl:w-2/6"
+            src={Hero}
+            alt=""
+          />
         </div>
 
-        <div className="mt-9 w-2/5">
-          <span className="text-gray-50 text-5xl font-bold block text-center">
+        <div className="mt-9 w-4/5 md:w-2/4 xl:w-2/5">
+          <span className="text-gray-50 text-3xl md:text-4xl xl:text-5xl font-bold block text-center">
             Find Movies You’ll Love Without the Hassle
           </span>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,28 @@ import Herosection from "../components/Herosection";
 const Home = () => {
   return (
     <>
-    
       <div>
         <Herosection />
       </div>
+      <div className="bg-[#09041f] text-gray-50 ">
+        <div className="md:w-11/12  m-auto">
+          <h1 className="font-bold text-4xl lg:text-5xl ">Popular Page 1</h1>
 
-      Home
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2.5 md:gap-5 xl:gap-10 mt-9">
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+            <div className="h-96 bg-blue-500"></div>
+          </div>
+            </div>
+      </div>
     </>
   );
 };
